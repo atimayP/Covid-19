20de79c9ccb5944b430bb1cd8b33e9e45297a811
Atimay Pengchai <62240095+atimayP@users.noreply.github.com>
2020-04-27 02:20:53 +0700

    added index.css update II

--- country.js
+++ country.js
@@ -1,5 +1,5 @@
-$(function () {
-
+$(function(){
+    
     var urlParams = new URLSearchParams(window.location.search);
     console.log(urlParams.get('country'));
 
@@ -13,8 +13,8 @@ $(function () {
         var selectedCountry = result[country];
         console.log(selectedCountry);
 
-        for (var i = 0; i < selectedCountry.length; i++) {
-
+        for(var i=0;i<selectedCountry.length;i++){
+           
             var row = `<tr>
             <th scope="row">${selectedCountry[i].date}</th>
             <td>${selectedCountry[i].confirmed}</td>
@@ -22,13 +22,13 @@ $(function () {
             <td>${selectedCountry[i].recovered}</td>
           </tr>`
 
-            $("#data").append(row);
+          $("#data").append(row);
 
         }
-
+        
 
     });
 
 
 
-}) 
+})

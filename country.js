ccb2490d5f1b491b0387cef63a48a97837f086f5
Atimay Pengchai <62240095+atimayP@users.noreply.github.com>
2020-04-27 21:34:03 +0700

    add map and update index

--- country.js
+++ country.js
@@ -1,7 +1,21 @@
-$(function(){
-    
+var date = []
+var confirmed = []
+var deaths = []
+const recovered = []
+getdata()
+setTimeout(table,3500)
+setTimeout(chart,3500)
+// $(function(){
+   
+// })
+$('#country').hide()
+$('#myTable').hide()
+$('#data').hide()
+
+async function getdata(){
     var urlParams = new URLSearchParams(window.location.search);
     console.log(urlParams.get('country'));
+
 
     var country = urlParams.get('country');
     $("#country").html(country);
@@ -10,25 +24,60 @@ $(function(){
 
     $.getJSON(url, function (result) {
 
-        var selectedCountry = result[country];
+        var selectedCountry =result[country];
         console.log(selectedCountry);
 
         for(var i=0;i<selectedCountry.length;i++){
-           
-            var row = `<tr>
-            <th scope="row">${selectedCountry[i].date}</th>
-            <td>${selectedCountry[i].confirmed}</td>
-            <td>${selectedCountry[i].deaths}</td>
-            <td>${selectedCountry[i].recovered}</td>
-          </tr>`
-
-          $("#data").append(row);
-
+           date[i]=selectedCountry[i].date
+           confirmed[i]=selectedCountry[i].confirmed
+           deaths[i]=selectedCountry[i].deaths
+           recovered[i]=selectedCountry[i].recovered
         }
-        
-
     });
+}
 
+async function table() {
 
+    console.log(date[0]);
+    console.log(confirmed);
+    console.log(date.length);
+    
+    
+    for(var i=date.length-1;i>0;i--){
+         var row = `<tr>
+         <th scope="row">${date[i]}</th>
+         <td>${confirmed[i]}</td>
+         <td>${deaths[i]}</td>
+         <td>${recovered[i]}</td>
+         </tr>`
+       $("#data").append(row);
 
-})
+     }
+     $('#country').show()
+     $('#loading').hide()
+     $('#myTable').show()
+     $('#data').show()
+}
+
+function chart(){
+    const ctx = document.getElementById("myChart");
+    ctx.height = 500;
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: date,
+                datasets: [{
+                    label: 'confirmed case',
+                    fill: false,
+                    data: confirmed,
+                    backgroundColor: 
+                        'rgba(186, 161, 212, 0.2)'
+                    ,
+                    borderColor: 
+                        'rgba(51, 0, 102)'
+                    ,
+                    borderWidth: 1
+                }]
+            }
+        });
+    }
